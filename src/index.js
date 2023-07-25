9dd6f5cc38b51a47a5c75a3405a704d60e0d715e
odogun maryam aderonke <odogunmaryam@gmail.com>
2023-07-25 12:48:16 +0100

    gameboard created

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import GameBoard from './Component/GameBoard';
+import App from './Component/App';
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
+
+
+
